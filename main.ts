a013ecb6d3c4996a1357b5c71cddce4d982681b6
Zbigniew Jerzak <ZbigniewJerzak@users.noreply.github.com>
2021-11-19 23:27:15 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P2, 29)
+    pins.servoWritePin(AnalogPin.P2, 0)
 })
-let avgLautstaerke = 0
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P2, 180)
+})
+let avgLaut = 0
+let currLaut = 0
 let _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
 _4digit.set(7)
 _4digit.clear()
@@ -9,24 +13,25 @@ let winkel = 30
 pins.servoWritePin(AnalogPin.P2, winkel)
 _4digit.show(Math.trunc(input.soundLevel()))
 basic.forever(function () {
-    avgLautstaerke = input.soundLevel()
-    _4digit.show(avgLautstaerke)
-    if (avgLautstaerke < 20) {
+    currLaut = input.soundLevel()
+    if (currLaut >= avgLaut) {
+        avgLaut = currLaut
+    } else {
+        avgLaut += 0 - 1
+    }
+    _4digit.show(avgLaut)
+    if (avgLaut < 20) {
         if (winkel != 30) {
             winkel = 30
             pins.servoWritePin(AnalogPin.P2, winkel)
         }
-    } else if (avgLautstaerke >= 20 && avgLautstaerke <= 60) {
+    } else if (avgLaut >= 20 && avgLaut <= 60) {
         if (winkel != 90) {
             winkel = 90
             pins.servoWritePin(AnalogPin.P2, winkel)
         }
-    } else if (avgLautstaerke > 60) {
+    } else if (avgLaut > 60) {
         if (winkel != 150) {
-            for (let Index = 0; Index <= 4; Index++) {
-                music.playTone(262, music.beat(BeatFraction.Sixteenth))
-                music.playTone(392, music.beat(BeatFraction.Sixteenth))
-            }
             winkel = 150
             pins.servoWritePin(AnalogPin.P2, winkel)
         }
